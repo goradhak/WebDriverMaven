3720bf615c47f34e422f31c503591db698f218f8
goradhak1 <goutham.rk@gmail.com>
2019-05-16 16:31:59 +0530

    Initial Commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,38 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	
+	
+	public static WebDriver driver;
+	public ChromeOptions o;
+	
+	@BeforeSuite
+	public void setUp(){
+		o = new ChromeOptions();
+		driver = new ChromeDriver(o);
+		
+	}
+	
+	@Test
+	public void doLogin(){
+		driver.get("http://gmail.com");
+		driver.findElement(By.name("identifier")).sendKeys("trainer@way2automation.com");
+		//driver.findElement(By.xpath(span(text(),"Next"))).click();
+		//driver.findElement(By.id("password")).sendKeys("sdfdf");
+	}
+	
+	@AfterSuite
+	public void tearDown(){
+		driver.quit();
+	}
+
+}
